3dca7d802a49f3fdaf79079efbb7a6b39eeadbcf
md.nur <63946435+Md-Nur@users.noreply.github.com>
2021-07-08 22:00:04 +0600

    1st Commit

---
+++ sp500-app.py
@@ -0,0 +1,94 @@
+import base64
+import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
+import yfinance as yf
+
+st.title('***st.title()*** S&P 500 App')
+
+st.markdown(""" ***st.markdown()*** 
+This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
+* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, yfinance
+* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
+""")
+
+st.sidebar.header('***st.sidebar.header()*** User Input Features')
+
+# Web scraping of S&P 500 data
+#
+
+
+@st.cache
+def load_data():
+    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    html = pd.read_html(url, header=0)
+    df = html[0]
+    return df
+
+
+df = load_data()
+sector = df.groupby('GICS Sector')
+
+# Sidebar - Sector selection
+sorted_sector_unique = sorted(df['GICS Sector'].unique())
+selected_sector = st.sidebar.multiselect(
+    '***st.sidebar()*** Sector', sorted_sector_unique, sorted_sector_unique)
+
+# Filtering data
+df_selected_sector = df[(df['GICS Sector'].isin(selected_sector))]
+
+st.header('***st.header()*** Display Companies in Selected Sector')
+st.write('***st.write()*** Data Dimension: ' + str(df_selected_sector.shape[0]) + ' rows and ' + str(
+    df_selected_sector.shape[1]) + ' columns. ***st.dataframe()***')
+st.dataframe(df_selected_sector)
+
+# Download S&P500 data
+# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
+
+
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    # strings <-> bytes conversions
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File ***st.markdown(a tag,unsafe_allow_html=True)*** </a>'
+    return href
+
+
+st.markdown(filedownload(df_selected_sector), unsafe_allow_html=True)
+
+# https://pypi.org/project/yfinance/
+
+data = yf.download(
+    tickers=list(df_selected_sector[:10].Symbol),
+    period="ytd",
+    interval="1d",
+    group_by='ticker',
+    auto_adjust=True,
+    prepost=True,
+    threads=True,
+    proxy=None
+)
+
+# Plot Closing Price of Query Symbol
+
+
+def price_plot(symbol):
+    df = pd.DataFrame(data[symbol].Close)
+    df['Date'] = df.index
+    plt.fill_between(df.Date, df.Close, color='skyblue', alpha=0.3)
+    plt.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
+    plt.xticks(rotation=90)
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    plt.title(symbol, fontweight='bold')
+    plt.xlabel('Date ***st.pyplot()***', fontweight='bold')
+    plt.ylabel('Closing Price', fontweight='bold')
+    return st.pyplot()
+
+
+num_company = st.sidebar.slider(
+    '***st.sidebar.slider()*** Number of Companies', 1, 5)
+
+# if st.button('Show Plots ***st.button()***'):
+st.header('Stock Closing Price ***st.header()***')
+for i in list(df_selected_sector.Symbol)[:num_company]:
+    price_plot(i)
